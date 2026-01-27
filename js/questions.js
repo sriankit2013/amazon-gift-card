f0d163082dbaf2d6c8400f9d1d97f429b907b48d
sriankit2013 <66566106+sriankit2013@users.noreply.github.com>
2026-01-28 02:59:00 +0530

    Create questions.js

---
+++ js/questions.js
@@ -0,0 +1,77 @@
+const surveyQuestions = {
+    restaurant: [
+        `Have you eaten at ${brandName} in the last 3 months?`,
+        `Do you follow ${brandName} on social media?`,
+        "Are you over the age of 18?"
+    ],
+    store: [
+        `Have you shopped at ${brandName} in the last 6 months?`,
+        `Would you recommend ${brandName} to a friend?`,
+        "Are you ready to claim your $1,000 Gift Card today?"
+    ],
+    generic: [
+        "Do you like online offers?",
+        "Would you complete short surveys?",
+        "Do you want to claim a reward today?"
+    ],
+    grocery: [
+        `Do you purchase groceries at stores like ${brandName}?`,
+        `Have you shopped at ${brandName} in the last 7 months?`,
+        `Are you interested in a $1,000 ${brandName} voucher?`
+    ],
+    finance: [
+        `Have you used ${brandName} for sending or receiving money?`,
+        `Would you recommend ${brandName} to friends or family?`,
+        "Are you over the age of 18?"
+    ],
+    fastfood: [
+        `Do you visit ${brandName} more than once a month?`,
+        `Would you rate your last experience at ${brandName} as positive?`,
+        `Are you interested in a $1,000 ${brandName} voucher?`
+    ],
+    dollarstore: [
+        `Do you shop at discount stores like ${brandName}?`,
+        `Do you feel ${brandName} offers great value for money?`,
+        `Are you interested in a $1,000 ${brandName} gift card?`
+    ],
+    gas: [
+        `Do you fill your tank everytime you get gas?`,
+        `Do you own or lease your vehicle?`,
+        `Are you interested in a $1,000 Gas Gift Card?`
+    ],
+    stim: [
+        `Are you an American citizen?`,
+        `Have you ever qualified for a stimulus check?`,
+        `Are you 18 years of age or older?`
+    ],
+    restaurant: [
+        `Do you eat out at least once a month?`,
+        `Do you prefer casual dining over fast food?`,
+        `Would you be interested in a restaurant gift card?`
+    ],
+    creditcard: [
+        `Do you currently own or use a ${brandName} card?`,
+        `Do you trust ${brandName} for your everyday purchases?`,
+        `Are you interested in exclusive ${brandName} rewards?`
+    ],
+    prizebox: [
+        `Have you ever received an Amazon Gift Box?`,
+        `Do you have an Amazon Prime account?`,
+        `Would you like to receive a free Amazon Prize Box?`
+    ],
+    shoes: [
+        `Have you ever owned a pair of ${brandName} shoes?`,
+        `Do you purchase athletic footwear at least once a year?`,
+        `Are you interested in a $1,000 ${brandName} gift card?`
+    ],
+    baby: [
+        `Do you use ${brandName} products regularly?`,
+        `Do you have children under the age of 5?`,
+        `Are you interested in free ${brandName} samples?`
+    ],
+    product: [
+        `Do you regularly purchase ${brandName} products?`,
+        `Would you recommend ${brandName} to friends or family?`,
+        `Are you interested in exclusive ${brandName} offers?`
+    ],
+};
